e981a4b2a95d90fda40a718146bbdf3ab4ffadf7
sootecc <sootecc@gmail.com>
2022-03-23 21:21:07 +0900

    페이지 맨 위로 가는 버튼

--- javascript/script.js
+++ javascript/script.js
@@ -70,3 +70,18 @@ $(function(){
         $("#popup").fadeOut(); //페이드아웃 효과
     }
 });
+
+
+
+//최상단으로 가는 버튼
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 300){
+		$('.btn_gotop').show();
+	} else{
+		$('.btn_gotop').hide();
+	}
+});
+$('.btn_gotop').click(function(){
+	$('html, body').animate({scrollTop:0},400);
+	return false;
+});
