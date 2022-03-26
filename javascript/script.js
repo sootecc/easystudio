d1d639dbb79bbec43002bc0865e7ea2aaa0bfc54
sootecc <sootecc@gmail.com>
2022-03-26 15:31:35 +0900

    디테일 페이지 5개, index 최하단 자동 슬라이드

--- javascript/script.js
+++ javascript/script.js
@@ -106,6 +106,61 @@ new Swiper('.myswiper', {
 
 });
 
+//자동 슬라이드
+var idx_lgth = $("#visual>div").length;
+var srt = 1;
+var idx = 0;
+
+$("section>a").click(function(){
+idx = $(this).index();
+srt = idx;
+$(this).addClass('on').siblings().removeClass('on');
+$("#visual>div").eq(idx).addClass('on').siblings().removeClass('on');
+});
+
+var AutoSlide = setInterval(AutoRun, 3000);
+
+function AutoRun(){
+srt++;  
+
+if(srt == idx_lgth){
+    srt = 0;  
+}
+$("section>a").eq(srt).addClass('on').siblings().removeClass('on');
+$("#visual>div").eq(srt).addClass('on').siblings().removeClass('on');
+}
+
+$("#visual").mouseover(function(){
+clearInterval(AutoSlide);
+$("#right").css({opacity : '1'});
+$("#left").css({opacity : '1'});
+});
+$("#visual").mouseleave(function(){
+AutoSlide = setInterval(AutoRun, 3000);     
+$("#right").css({opacity : '0'});
+$("#left").css({opacity : '0'});
+});
+
+$("#right").click(function(){
+srt++;
+if(srt == idx_lgth){
+    srt = 0;   
+} 
+$("#visual>div").eq(srt).addClass('on').siblings().removeClass('on'); $("section>a").eq(srt).addClass('on').siblings().removeClass('on');
+});
+
+$("#left").click(function(){
+srt--;
+if(srt == -1){
+    srt = idx_lgth-1; 
+}
+$("#visual>div").eq(srt).addClass('on').siblings().removeClass('on');
+$("section>a").eq(srt).addClass('on').siblings().removeClass('on'); 
+});
+
+
+
+
 //클릭 시 해당 엘리먼트로 스크롤 이동
 (document).ready(function($) {
 
