e8e80aa2c14ef89b08a2e4e9aae90d7de29a2b42
sootecc <sootecc@gmail.com>
2022-06-07 22:39:36 +0900

    main slide

--- javascript/index_slide.js
+++ javascript/index_slide.js
@@ -1,8 +1,4 @@
-// ※ 클래스명 myswiper 사용
-new Swiper('.myswiper', {
-
-	// 슬라이드 속도 설정
-	// 지정하지 않을시 기본값은 300
-	speed : 1000,
-
+Flipsnap('.flipsnap', {
+    distance: 230,
+    transitionDuration: 1000
 });
