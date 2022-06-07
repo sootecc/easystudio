6b75b3aebb191443d4bd16271e3e613163afbd9c
sootecc <sootecc@gmail.com>
2022-06-07 17:21:34 +0900

    detail

--- javascript/script.js
+++ javascript/script.js
@@ -14,7 +14,7 @@ $(window).scroll(function(){
 	}
 });
 $('.btn_gotop').click(function(){
-	$('html, body').animate({scrollTop:0},400);
+	$('html, body').animate({scrollTop:0},700);
 	return false;
 });
 
