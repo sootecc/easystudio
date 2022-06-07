f2fed96be9dd8a86a667b5833fae0c62353c0bf9
sootecc <sootecc@gmail.com>
2022-06-07 18:32:01 +0900

    1

--- javascript/review.js
+++ javascript/review.js
@@ -1,54 +1,58 @@
-let $grid = $('.grid').masonry({
-    itemSelector: '.photo-item',
-    columnWidth: '.grid__col-sizer',
-    gutter: '.grid__gutter-sizer',
-    percentPosition: true,
-    stagger: 30,
-    // nicer reveal transition
-    visibleStyle: { transform: 'translateY(0)', opacity: 1 },
-    hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
+// container 클래스안의 모든 이미지가 로딩되면 masonry 적용
+$imgs = $('.container').imagesLoaded(function() {
+  $imgs.masonry({
+      itemSelector : '.item', // img 태그를 대상으로 masonry 적용
+      fitWidth : true // 내용물을 가운데 정렬하기, CSS margin:0 auto; 설정이 필요함
   });
-  
-  //------------------//
-  
-  // Get an API key for your demos at https://unsplash.com/developers
-  const unsplashID = 'cwS5ht1X3GTDb0vCKPAObUYQtRsP7ctxSs9acZL3t7o';
-  
-  // get Masonry instance
-  var msnry = $grid.data('masonry');
-  
-  $grid.infiniteScroll({
-    path: function() {
-      return `https://api.unsplash.com/photos?client_id=${unsplashID}&page=${this.pageIndex}`;
-    },
-    // load response as JSON
-    responseBody: 'json',
-    outlayer: msnry,
-    status: '.page-load-status',
-    history: false,
-  });
-  
-  $grid.on( 'load.infiniteScroll', function( event, body ) {
-    // compile body data into HTML
-    let itemsHTML = body.map( getItemHTML ).join('');
-    // convert HTML string into elements
-    let $items = $( itemsHTML );
-    // append item elements
-    $items.imagesLoaded( function() {
-      $grid.append( $items ).masonry( 'appended', $items );
-    })
-  });
-  
-  // load initial page
-  $grid.infiniteScroll('loadNextPage');
-  
-  //------------------//
-  
-  function getItemHTML({ user, urls }) {
-    return `<div class="photo-item">
-      <img class="photo-item__image" src="${urls.regular}" alt="Photo by ${user.name}" />
-      <p class="photo-item__caption">
-        <a href="${user.links.html}?utm_source=infinite-scroll-demos&utm_medium=referral&utm_campaign=api-credit">${user.name}</a>
-      </p>
-    </div>`;
+});
+
+
+
+function indexViewMore() {
+  var lastIndex = 0;
+  if ((currentIndex + 10) < rankList.length) {
+      lastIndex = currentIndex + 10;
+  } else {
+      lastIndex = rankList.length;
   }
+
+  var htmlString = '';
+  for (currentIndex; currentIndex < lastIndex; currentIndex++) {
+      htmlString += '<div class="item">';
+      htmlString += '    <img src="' + apiUrl + rankList[currentIndex]["thumbnailImageUrl"] + '" class="gg-image" onclick="javascript:goGallery(\'' + rankList[currentIndex]["userId"] + '\', \'' + rankList[currentIndex]["userName"] + '\', \'' + rankList[currentIndex]["countryCode"] + '\', \'' + rankList[currentIndex]["Cnt"] + '\', \'' + rankList[currentIndex]["photoUrl"] + '\', \'0\', \'' + rankList[currentIndex]["id"] + '\');">';
+      htmlString += '    <div class="info">';
+      htmlString += '        <div class="donee donee-' + rankList[currentIndex]["givingTargetId"] + '"></div>';
+      htmlString += '        <div class="profile">';
+
+      if (rankList[currentIndex]["photoUrl"]) {
+          htmlString += '         <img src="' + apiUrl + rankList[currentIndex]["photoUrl"] + '">';
+      } else {
+          htmlString += '         <img class="null" src="">';
+      }
+      
+      htmlString += '        </div>';
+      htmlString += '        <div class="username">' + rankList[currentIndex]["userName"] + '</div>';
+      htmlString += '        <div class="country">' + countryName(rankList[currentIndex]["countryCode"]) + '</div>';
+      htmlString += '        <div class="tag-area">';
+
+      for (var j = 0; j < rankList[currentIndex]["hashTags"].length; j++) {
+          htmlString += '            <a class="tag">#' + rankList[currentIndex]["hashTags"][j] + '</a>';
+      }//end for j
+
+      htmlString += '        </div>';
+      htmlString += '    </div>';
+      htmlString += '</div>';
+  }//end for currentIndex
+
+  var localEl = $(htmlString);
+
+  $('#masonryContainer').append(localEl).masonry('appended', localEl, true);
+
+  setTimeout(function() {
+      $('#masonryContainer').masonry();
+  }, 50);
+
+  setTimeout(function() {
+      $('#masonryContainer').masonry();
+  }, 50);
+}
