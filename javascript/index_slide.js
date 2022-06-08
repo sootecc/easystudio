ba0c7e180fb332efdae096bf155ec9c700f69e5a
sootecc <sootecc@gmail.com>
2022-06-08 16:18:14 +0900

    index

--- javascript/index_slide.js
+++ javascript/index_slide.js
@@ -2,3 +2,13 @@ Flipsnap('.flipsnap', {
     distance: 230,
     transitionDuration: 1000
 });
+
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+	direction: "vertical",
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
